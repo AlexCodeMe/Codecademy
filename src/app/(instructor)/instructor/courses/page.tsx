83260b7cf07be0dc6f5148a337c6076a48d9db8f
Alex Moyer <alexmoyer@Alexs-MacBook-Air.local>
2024-06-19 12:57:32 -0400

    db

--- src/app/(instructor)/instructor/courses/page.tsx
+++ src/app/(instructor)/instructor/courses/page.tsx
@@ -1,7 +1,17 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 export default function CoursePage() {
-  return (
-    <div>CoursePage</div>
-  )
+    return (
+        <div className="px-6 py-4">
+            <Link href="/instructor/create-course">
+                <Button>Create New Course</Button>
+            </Link>
+
+            <div className="mt-5">
+                {/* <DataTable columns={columns} data={courses} /> */}
+            </div>
+        </div>
+    )
 }
